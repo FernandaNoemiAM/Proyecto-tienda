41e591e574b23e4ac93319b4ef6c5b6d899d2d49
CesarDanUNAM <42225767+CesarDanUNAM@users.noreply.github.com>
2018-10-19 21:54:16 -0500

    Versiones anteriores

---
+++ Login v.3/Login/User Data.swift
@@ -0,0 +1,20 @@
+//
+//  User Dara.swift
+//  Login
+//
+//  Created by Usuario invitado on 9/10/18.
+//  Copyright © 2018 1. All rights reserved.
+//
+
+import Foundation
+struct UserData {
+    var firsname: String
+    var lastname: String
+    var username: String
+    var password: String
+}
+
+var save = [UserData]()
+
+
+
